cb4e1c5429c7499a1cf3eb90ad806136a2aa9c2a
Mateusz <M.M.Redzimski@gmail.com>
2024-06-05 02:08:54 +0200

    Collision

--- game.py
+++ game.py
@@ -7,7 +7,8 @@ from player import Player
 import sprites
 
 pygame.init()
-
+pygame.font.init()
+pygame.mixer.init()
 
 class Game:
     def __init__(self):
@@ -28,6 +29,7 @@ class Game:
         self.bg_sprite = sprites.sprite.get_sprite('BG')
         self.bg_width, self.bg_height = self.bg_sprite.get_size()
         self.how_many_bgs = 1
+        self.current_level = 1
         self.camera = None
         self.player = None
 
@@ -39,8 +41,14 @@ class Game:
         self.start_x, self.start_y, self.y_speed, self.x_speed = 100, 100, 0, 0
         self.running = True
         self.delay = 0
+        self.time = 9999
+        self.level_time = 9999
 
         self.load(config["entry_level"])
+        music_path = "music/music.mp3"
+        pygame.mixer.music.load(music_path)
+        pygame.mixer.music.set_volume(0.1)
+        pygame.mixer.music.play()
 
     def start(self):
         while self.running:
@@ -67,6 +75,8 @@ class Game:
             self.player.draw(screen=self.canvas)
 
             self.camera.surface.blit(self.canvas.subsurface(self.camera.rect), (0, 0))
+            textsurface = self.font.render(f'Time left: {self.time:.1f}', True, RED)
+            self.camera.surface.blit(textsurface, (self.width/2 - 50, 200))
             self.screen.blit(pygame.transform.smoothscale(self.camera.surface, (self.width, self.height)), (0, 0))
 
             pygame.display.update()
@@ -75,19 +85,25 @@ class Game:
                 self.canvas.blit(self.bg_sprite, (i*self.bg_width, 0))
             # pygame.draw.rect(self.canvas, RED, self.camera.rect, 0)
             self.delay = self.clock.tick(self.max_fps) / 1000
+            self.time -= self.delay
+            if self.time < 0:
+                self.restart
+            self.check_player_position()
 
     def load(self, level_str):
+        self.blocks = dict()
         with open(f"levels/{level_str}.json", "r") as file:
             level = json.load(file)
         self.level_cols = level["cols"]
         self.level_rows = level["rows"]
         self.level_block_size = level["block_size"]
         self.start_x, self.start_y = level["start"]["x"], level["start"]["y"]
+        self.level_time = self.time = level["time"]
         level_width = self.level_cols * self.level_block_size
         level_height = self.level_rows * self.level_block_size
         self.canvas = pygame.Surface((level_width, level_height))
         self.how_many_bgs = level_width // self.bg_width + 1
-        self.camera = Camera(self.screen.get_size(),self.canvas.get_size(), self.level_block_size)
+        self.camera = Camera(self.screen.get_size(), self.canvas.get_size(), self.level_block_size)
 
         self.player = Player(self.start_x, self.start_y, 32, 32, block_size=self.level_block_size, blocks=None)
 
@@ -104,28 +120,49 @@ class Game:
 
     def add_block(self, block):
         name, x, y, w, h = block["name"], block["x"], block["y"], block["w"], block["h"]
+        collision = block["collision"] if "collision" in block else None
+        if collision == "die":
+            collision = self.restart
+        if collision == "win":
+            collision = self.level_up
+
+        overflow_x = block["overflow_x"] if "overflow_x" in block else 0
+        overflow_y = block["overflow_y"] if "overflow_y" in block else 0
         for row in range(h):
             for col in range(w):
                 curr_x = x + col
                 curr_y = y + row
                 if curr_x not in self.blocks.keys():
                     self.blocks[curr_x] = list()
-                self.blocks[curr_x].append((name, curr_x, curr_y))
+                self.blocks[curr_x].append((name, collision, overflow_x, overflow_y, curr_x, curr_y))
 
     def draw_blocks(self):
         for col in self.blocks.keys():
             for block_tuple in self.blocks[col]:
-                name = block_tuple[0]
-                x = block_tuple[-2] * self.level_block_size
-                y = block_tuple[-1] * self.level_block_size
+                name, collision, overflow_x, overflow_y, curr_x, curr_y = block_tuple
+                x = curr_x * self.level_block_size + overflow_x
+                y = curr_y * self.level_block_size + overflow_y
                 block_sprite = sprites.sprite.get_sprite(name)
                 self.canvas.blit(block_sprite, (x, y))
     @staticmethod
     def follow(camera_pos, player_pos, speed):
         return camera_pos + (player_pos - camera_pos) * speed
 
+    def check_player_position(self):
+        canvas_width, canvas_height = self.canvas.get_size()
+        if self.player.x > canvas_width:
+            self.restart()
+        elif self.player.y > canvas_height:
+            self.restart()
+        elif self.player.x + self.player .width < 0:
+            self.restart()
+
+    def level_up(self):
+        self.current_level += 1
+        self.load('002')
 
     def restart(self):
+        self.time = self.level_time
         self.player.x = self.start_x
         self.player.y = self.start_y
         self.player.x_speed = self.x_speed
--- player.py
+++ player.py
@@ -13,6 +13,9 @@ class Player:
         self.blocks = blocks
         self.right_pressed = False
         self.left_pressed = False
+        self.pl, self.pr, self.pd, self.pu = None, None, None, None
+        self.jump_time, self.fall_time = 0, 0
+        self.flipped = False
 
         self.status = Status(S_IDLE, self)
 
@@ -22,15 +25,23 @@ class Player:
         self.left_pressed = bool(keys[pygame.K_LEFT])
         if self.left_pressed:
             self.x_speed = -MOVE_SPEED
-        if self.right_pressed:
+            self.flipped = True
+        elif self.right_pressed:
             self.x_speed = MOVE_SPEED
+            self.flipped = False
         if not self.left_pressed and not self.right_pressed:
             self.x_speed = 0
-
+    def update_player_dirs(self):
+        d = 8
+        self.pl = pygame.Rect(self.x, self.y + d, d, self.height - 2 * d)
+        self.pr = pygame.Rect(self.x + self.width - d, self.y + d, d, self.height - 2 * d)
+        self.pd = pygame.Rect(self.x + d, self.y + self.height - d, self.width - 2 * d, d)
+        self.pu = pygame.Rect(self.x + d, self.y, self.width - 2 * d, d)
     def update(self, delay):
         self.delay = delay
         self.x += self.x_speed * delay
         self.y += self.y_speed * delay
+        self.update_player_dirs()
         self.status.update(self, self.delay)
         moves = []
         if self.x_speed > 0:
@@ -48,7 +59,7 @@ class Player:
     def draw(self, screen):
         new_index = self.status.index % self.status.count
         sprite_name = self.sprite + str(new_index)
-        sprite = sprites.sprite.get_sprite(sprite_name)
+        sprite = sprites.sprite.get_sprite(sprite_name, self.flipped)
         screen.blit(sprite, (self.x, self.y))
 
     def get_player_cols(self):
@@ -70,9 +81,6 @@ class Player:
     def update_blocks(self, blocks):
         self.blocks = blocks
 
-    def get_player_rect(self):
-        return pygame.Rect(self.x, self.y, self.width, self.height)
-
 class Status:
     def __init__(self, status_name: str, player: Player):
         self.name = status_name
@@ -84,6 +92,7 @@ class Status:
             self.keyboard = self.idle_keyboard
             self.update = self.idle_update
         if self.name == S_FALL:
+            player.fall_time = 0
             self.index, self.count, self.elapsed = self.fall_props()
             self.player.sprite = S_FALL
             self.keyboard = self.fall_keyboard
@@ -95,6 +104,7 @@ class Status:
             self.keyboard = self.run_keyboard
             self.update = self.run_update
         if self.name == S_JUMP:
+            player.jump_time = 0
             self.index, self.count, self.elapsed = self.jump_props()
             self.player.sprite = S_JUMP
             self.keyboard = self.jump_keyboard
@@ -105,32 +115,48 @@ class Status:
         self.block_up_player = [0, 1]
         self.block_left_player = [1, 0]
 
-    def check_collision(self, side_b):
+    def check_collision(self, side_b, p_rect, run=False):
 
         blocks = self.player.get_possible_collision_blocks()
         bs = self.block_size
+        player_run_right = self.player.x_speed > 0
         for block in blocks:
-            block_rect = pygame.Rect(block[-2]*bs + side_b[0],
-                                     block[-1] * bs + side_b[1],
+            name, collision, overflow_x, overflow_y, curr_x, curr_y, = block
+            if run:
+                if player_run_right:
+                    block_rect = pygame.Rect(curr_x * bs + side_b[0] + overflow_x,
+                                             curr_y * bs + side_b[1] + overflow_y,
+                                             bs+STILL_RUN,
+                                             bs)
+                else:
+                    block_rect = pygame.Rect(curr_x * bs + side_b[0] + overflow_x - STILL_RUN,
+                                             curr_y * bs + side_b[1] + overflow_y,
+                                             bs+STILL_RUN,
+                                             bs)
+            else:
+                block_rect = pygame.Rect(curr_x*bs + side_b[0]+overflow_x,
+                                     curr_y *bs + side_b[1]+overflow_y,
                                      bs,
                                      bs)
-            p_rect = self.player.get_player_rect()
             if p_rect.colliderect(block_rect):
+                if collision is not None:
+                    collision()
+                    return False, None
                 return True, block
                 break
         return False, None
 
-    def common_update_left(self):
+    def common_update_left(self, p_rect):
         if self.player.left_pressed:
-            collision, block = self.check_collision(self.block_left_player)
+            collision, block = self.check_collision(self.block_left_player, p_rect)
             if collision:
                 self.player.x_speed = 0
                 self.player.x = block[-2] * self.block_size + self.block_size
                 return block
 
-    def common_update_right(self):
+    def common_update_right(self, p_rect):
         if self.player.right_pressed:
-            collision, block = self.check_collision(self.block_right_player)
+            collision, block = self.check_collision(self.block_right_player, p_rect)
             if collision:
                 self.player.x_speed = 0
                 self.player.x = block[-2] * self.block_size - self.player.width
@@ -147,12 +173,12 @@ class Status:
         if keys[pygame.K_UP]:
             player.status = Status(S_JUMP, player)
             player.y_speed = -JUMP_SPEED
-        if keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]:
+        elif keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]:
                 player.status = Status(S_RUN, player)
 
     def idle_update(self, player: Player, delay):
-        self.common_update_left()
-        self.common_update_right()
+        self.common_update_left(player.pl)
+        self.common_update_right(player.pr)
         player.x_speed = 0
         player.y_speed = 0
         self.elapsed += delay
@@ -160,7 +186,7 @@ class Status:
             self.index += 1
             self.elapsed = 0
 
-        collision, block = self.check_collision(self.block_under_player)
+        collision, block = self.check_collision(self.block_under_player, player.pd)
 
         if not collision:
             player.y_speed = 10
@@ -176,20 +202,24 @@ class Status:
     def fall_keyboard(self, player: Player, keys):
         if keys[pygame.K_DOWN]:
             self.add_gravity = 100
+        else:
+            self.add_gravity = 0
 
     def fall_update(self, player: Player, delay):
-        self.common_update_left()
-        self.common_update_right()
-        self.player.y_speed += (GRAVITY + self.add_gravity) * delay
+        self.common_update_left(player.pl)
+        self.common_update_right(player.pr)
+        player.fall_time += delay
+        self.player.y_speed += (GRAVITY + self.add_gravity) * player.fall_time/10
         self.elapsed += delay
         if self.elapsed > 0.05:
             self.index += 1
             self.elapsed = 0
 
-        collision, block = self.check_collision(self.block_under_player)
+        collision, block = self.check_collision(self.block_under_player, player.pd)
 
         if collision:
             player.y_speed = 0
+            player.y = block[-1] * self.block_size - player.height
             player.status = Status(S_IDLE, player)
     # DEATH
 
@@ -214,13 +244,18 @@ class Status:
             player.status = Status(S_IDLE, player)
 
     def run_update(self, player: Player, delay):
-        self.common_update_left()
-        self.common_update_right()
+        self.common_update_left(player.pl)
+        self.common_update_right(player.pr)
         self.elapsed += delay
         if self.elapsed > 0.05:
             self.index += 1
             self.elapsed = 0
-        # collision check
+
+        collision, block = self.check_collision(self.block_under_player, player.pd, True)
+
+        if not collision:
+            player.y_speed = 10
+            player.status = Status(S_FALL, player)
 
     # JUMP
 
@@ -232,17 +267,21 @@ class Status:
     @staticmethod
     def jump_keyboard(self, player: Player, keys):
         if keys[pygame.K_DOWN]:
+            player.y_speed = 0
             player.status = Status(S_FALL, player)
 
     def jump_update(self, player: Player, delay):
-        self.common_update_left()
-        self.common_update_right()
+        self.common_update_left(player.pl)
+        self.common_update_right(player.pr)
+        player.jump_time += delay
         self.elapsed += delay
         if self.elapsed > 0.05:
             self.index += 1
             self.elapsed = 0
-        player.y_speed = -JUMP_SPEED
-        collision, block = self.check_collision(self.block_up_player)
-        if collision:
+
+        collision, block = self.check_collision(self.block_up_player, player.pu)
+
+        player.y_speed = -JUMP_SPEED + (GRAVITY * player.jump_time)
+        if collision or player.y_speed >= 0:
             player.y_speed = 0
             player.status = Status(S_FALL, player)
